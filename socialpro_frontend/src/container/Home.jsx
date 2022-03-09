586be6156fc2ac63ba85c5a08adde70fcb5ceb8e
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2022-03-09 17:19:17 +0100

    created the Pin component with hover effects and the data from the user and the download function

--- socialpro_frontend/src/container/Home.jsx
+++ socialpro_frontend/src/container/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { HiMenu } from 'react-icons/hi';
 import { AiFillCloseCircle } from 'react-icons/ai';
 import { Link, Route, Routes } from 'react-router-dom';
+import {fetchUser} from '../utils/fetchUser';
 
 import { Sidebar, UserProfile } from '../components';
 import { userQuery } from '../utils/data';
@@ -14,7 +15,7 @@ const Home = () => {
   const [user, setUser] = useState();
   const scrollRef = useRef(null);
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
+  const userInfo = fetchUser();
 
   useEffect(() => {
     const query = userQuery(userInfo?.googleId);
