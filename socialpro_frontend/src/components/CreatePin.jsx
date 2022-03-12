b554495b8f4b8187ecdd11f11cde21c97475732a
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2022-03-12 17:14:19 +0100

    added the connection to sanity in the create-pin component

--- socialpro_frontend/src/components/CreatePin.jsx
+++ socialpro_frontend/src/components/CreatePin.jsx
@@ -13,7 +13,7 @@ const CreatePin = ({user}) => {
   const [about, setAbout] = useState('');
   const [destination, setDestination] = useState('');
   const [loading, setLoading] = useState(false);
-  const [fields, setField] = useState(null);
+  const [fields, setFields] = useState(null);
   const [category, setCategory] = useState(null);
   const [imageAsset, setImageAsset] = useState(null);
   const [wrongImageType, setWrongImageType] = useState(false);
@@ -40,6 +40,38 @@ const CreatePin = ({user}) => {
         setWrongImageType(true);
       }
   }
+
+  const savePin = () => {
+    if(title && about && destination && imageAsset?._id && category) {
+        const doc = {
+          _type: 'pin',
+          title,
+          about,
+          destination,
+          image: {
+            _type: 'image', 
+            asset: {
+              _type: 'reference',
+              _ref: imageAsset?._id       
+            }
+          },
+          userId: user._id,
+          postedBy: {
+            _type: 'postedBy',
+            _ref: user._id,
+          },
+          category,
+        }
+        client.creat(doc)
+        .then(() => {
+          navigate('/')
+        })
+    } else {
+      setFields(true);
+
+      setTimeout(() => setFields(false), 2000)
+    }
+  }  
 
 
   return (
@@ -125,6 +157,28 @@ const CreatePin = ({user}) => {
         <div className="flex flex-col">
           <div>
             <p className="mb-2 font-semibold tex-lg sm:text-xl">Choose Pin Categories</p>
+            <select
+              onChange={(e) => setCategory(e.target.value)}
+              className="outline-none w-4/5 text-base border-b-2 border-gray-200 p-2 rounded-md cursor-pointer"
+            >
+              <option value="other" className="bg-white"> Select Category</option>
+
+                {categories.map((category) => (
+                  <option className="text-base border-0 outline-none capitalize bg-white text-black" value={category.name}>
+                    {category.name}
+                  </option>
+                ))}
+                
+            </select>
+            <div className="flex justify-end items-end mt-5">
+                  <buttton
+                  type="button"
+                  onClick={savePin}
+                  className="bg-red-500 text-white font-bold p-2 rounded-full w-28 outline-none"
+                  >
+                  Save Pin
+                  </buttton>
+            </div>
           </div>
         </div>
         </div>
