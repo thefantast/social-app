45e24d890c418bd8391894cecf4c387577950371
David Bründel <davidbrundel@MacBook-Pro-von-David.local>
2022-03-14 17:36:14 +0100

    now I added the pin detail page with the comment section. the comment section is now connected to the sanity client

--- socialpro_frontend/src/components/PinDetail.jsx
+++ socialpro_frontend/src/components/PinDetail.jsx
@@ -21,24 +21,163 @@ const PinDetail = ({ user }) => {
   // from pins in the dynamic path like :pinId. Use useParams to get the user data from there
   const { pinId } = useParams();
 
+// here we put the comments from the user to the sanity cms
+//For that we use also for keys the uuidv4 function, this will give each comment a specific id
+//when you insert something you have to commit the insert.
+  const addComment = () => {
+    if(comment) {
+      setAddingComment(true);
+
+      client
+      .patch(pinId)
+      .setIfMissing({comments: []})
+      .insert('after', 'comments[-1]', [{
+        comment,
+        _key: uuidv4(),
+        postedBy: {
+           _type: 'postedBy',
+           _ref: user._id
+        }
+      }] )
+      .commit()
+      .then(() => {
+        fetchPinDetails();
+        setComment('');
+        setAddingComment(false);
+      }) 
+    }
+  }
+
+  const fetchPinDetails = () => {
+
+    let query = pinDetailQuery(pinId);
+
+    // this will return an array of the pins
+    // this will use as recommondation for other similar pins
+    if(query){
+      client.fetch(query)
+      .then((data) => {
+         setPinDetail(data[0]);
+
+         if(data[0]) {
+           query = pinDetailMorePinQuery(data[0]);
+
+           client.fetch(query)
+            .then((res) => setPins(res));
+         }
+      })
+    }
+  }
+ //use Effect only change when the pinId change
+  useEffect(() => {
+    fetchPinDetails();
+  }, [pinId])
+
   // add the loading indicator
 
   if(!pinDetail) return <Spinner message="Loading pin...." />
 
-  const fetchPinDetails = () => {
-    const query = pinDetailQuery(pinId);
-
-    // this will return an array of the pins
-    if(query){
-      client.fetch(query)
-      .then((data) => {
-         setPinDetail(data[0])
-      })
-    }
-  }
-
   return (
-    <div>PinDetail</div>
+    <>
+    <div className="flex xl-flex-row flex-col m-auto bg-white" style={{maxWidth: '1500px', borderRadius: '32px'}}>
+      <div className="flex justify-center items-center md:items-start flex-initial ">
+        <img
+          src={pinDetail?.image && urlFor(pinDetail.image).url()}
+          className="rounded-t-3xl rounded-b-lg"
+          alt="user-post"
+        />
+      </div>
+        <div className="w-full p-5 flex-1 xl:min-w-620">
+          <div className="flex items-center justify-between">
+            <div className="flex gap-2 items-center">
+              <a
+                href={`${pinDetail.image.asset.url}?dl=`}
+                download
+                onClick={(e) => {e.stopPropagation();
+                }}
+                className="bg-white w-9 h-9 p-2 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+              >
+              <MdDownloadForOffline />
+              </a>
+            </div>
+              <a href={pinDetail.destination} target="blank" rel="noreferrer" >
+                {
+                  pinDetail.destination
+                }
+              </a>
+          </div>
+            <div>
+                <h1 className="text-4xl font-bold break-words mt-3">
+                  {pinDetail.title}
+                </h1>
+                <p className="mt-3"> {pinDetail.description}</p>
+            </div>
+            <Link to={`user-profile/${user._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
+                <img
+                     className="w-8 h-8 rounded-full object-cover"
+                     src={pinDetail.postedBy.image}
+                     alt="user-profile"               
+                />
+                <p className="font-semibold capitalize">{pinDetail.postedBy.userName}</p>
+           </Link>
+           <h2 className="mt-5 tex-2xl">Comments</h2>
+           <div className="may-h-370 overflow-y-auto">
+                {pinDetail?.comments?.map((comment, i) => (
+                  <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+                    <img
+                    
+                      src={comment.postedBy.image}
+                      alt="user-profile"
+                      className="w-10 h-10 rounded-full cursor-pointer "
+                    />
+                    {/* now we have to define who posted the comment */}
+                    <div className="flex flex-col">
+                        <p className="font-bold">{comment.postedBy.userName}</p>
+                        <p>{comment.comment}</p>
+                    </div>
+                  </div>
+                ))}
+           </div>
+           <div className="flex flex-wrap mt-6 gap-3">
+            <Link to={`user-profile/${user._id}`} >
+              <img
+                className="w-10 h-10 rounded-full cursor-pointer"
+                src={pinDetail.postedBy.image}
+                alt="user-profile"               
+              />
+             <p className="font-semibold capitalize">{pinDetail.postedBy.userName}</p>
+            </Link>
+            <input
+               className="flex-1 border-gray-100 outline-none border-2 p-2 rounded-2xl focus: border-gray-300"
+               type="text"
+               placeholder="Add a comment..."
+               value={comment}
+               onChange={(e) => setComment(e.target.value)}
+            />
+            <button
+            type="button"
+            className="bg-red-500 text-white rounded-full px-6 py-2 font-semibold text-base outline-none "
+            onClick={addComment}
+            >
+            {addingComment ? 'Posting the comment...' : 'Post'}
+            </button>
+
+           </div>
+        </div>
+    </div>
+    {pins ? (
+      <>
+        <h2 className="text-center font-bold tex-2x mt-8 mb-4">
+          More like this
+        </h2>
+      <MasonryLayout pins={pins} />
+
+      </>
+
+    ): (
+      <Spinner message="Loading more pins" />
+    )}
+    </>
   )
 }
 
